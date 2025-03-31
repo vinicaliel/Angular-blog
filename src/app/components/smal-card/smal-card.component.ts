428ce505cb42685bcc885e1761c5cb482b0bbccf
Caliel Passos <caliel1023@yahoo.com>
2025-03-31 17:39:48 -0300

    chore components archtecture

---
+++ src/app/components/smal-card/smal-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-smal-card',
+  imports: [],
+  templateUrl: './smal-card.component.html',
+  styleUrl: './smal-card.component.css'
+})
+export class SmalCardComponent {
+
+}
